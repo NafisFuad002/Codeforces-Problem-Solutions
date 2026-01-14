eaa6ac1c65419228480cd1b72d6276b698254f8e
NafisFuad002 <nafisfuad162@gmail.com>
2026-01-15 01:23:43 +0600

    15.1.2026

---
+++ One and Two.cpp
@@ -0,0 +1,123 @@
+/*
+https://codeforces.com/problemset/problem/1788/A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nl cout<<"\n";
+#define pf cout<<
+#define sf cin>>
+#define ll long long
+#define py pf "YES"; nl
+#define pn pf "NO"; nl
+#define arrin for(int i=0; i<n; i++){sf arr[i];}
+#define arr2in for(int i=0; i<n; i++){sf arr2[i];}
+
+
+ll gcd(ll a,ll b);
+ll max_value_in_arr(ll st_ind, ll en_ind, ll arr[]);
+ll min_value_in_arr(ll st_ind, ll en_ind, ll arr[]);
+bool element_checker_in_array(char cha);
+
+
+void fun()
+{
+    int n, c=0;
+    sf n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        sf arr[i];
+        if(arr[i]==2)
+        {
+            c++;
+        }
+    }
+    if(c%2==1)
+    {
+        pf "-1";
+    }
+    else if(c==0)
+    {
+        pf "1";
+    }
+    else
+    {
+        c/=2;
+        for(int i=0; c>0; i++)
+        {
+            if(arr[i]==2)
+            {
+                c--;
+            }
+            if(c==0)
+            {
+                pf i+1;
+                break;
+            }
+        }
+    }
+    nl
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t=1;
+    sf t;
+    while(t>0)
+    {
+        fun();
+        t--;
+    }
+
+    return 0;
+}
+
+ll gcd(ll a,ll b)
+{
+    while(b!=0)
+    {
+        ll temp=b;
+        b=a%b;
+        a=temp;
+    }
+    return a;
+}
+ll max_value_in_arr(ll st_ind, ll en_ind, ll arr[])
+{
+    ll t=INT_MIN;
+    for(ll i=st_ind; i<=en_ind; i++)
+    {
+        if(t<arr[i])
+        {
+            t=arr[i];
+        }
+    }
+    return t;
+}
+ll min_value_in_arr(ll st_ind, ll en_ind, ll arr[])
+{
+    ll t=INT_MAX;
+    for(ll i=st_ind; i<=en_ind; i++)
+    {
+        if(t>arr[i])
+        {
+            t=arr[i];
+        }
+    }
+    return t;
+}
+bool element_checker_in_array(char cha)
+{
+    char arra[]= {'a','e','i','o','u','y'};
+    if(find(arra,arra+6,cha) != arra+6)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
